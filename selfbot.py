121a85ea6dffe2c79af286efa105f28fc883c32e
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-10-09 19:14:34 +0530

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -41,6 +41,25 @@ import textwrap
 from PIL import Image
 import io
 
+
+client = discord.Client()
+user = 'TqnZOPfoJsXVUNam'
+key = '46JYSuDaKLGZdNW1ipstUqVt5BaDVnAz'
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
+    await client.change_presence(game=discord.Game(name='chat with me!'))
+
+@client.event
+async def on_message(message):
+    if not message.author.bot and (message.server == None or client.user in message.mentions):
+        await client.send_typing(message.channel)
+        txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
+        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'kurumi', 'text':txt}).text)
+        if r['status'] == 'success':
+            await client.send_message(message.channel, r['response'] )
+            
 class Selfbot(commands.Bot):
     '''
     Custom Client for selfbot.py - Made by verix#7200
@@ -236,3 +255,6 @@ class Selfbot(commands.Bot):
 
 if __name__ == '__main__':
     Selfbot.init()
+    
+    print('Starting... cleverbot')
+requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'kurumi'})
