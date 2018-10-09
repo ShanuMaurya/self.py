0d0efe892ed516d609c6888f07cd5788c4e42237
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-10-09 19:33:50 +0530

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -46,19 +46,9 @@ client = discord.Client()
 user = 'TqnZOPfoJsXVUNam'
 key = '46JYSuDaKLGZdNW1ipstUqVt5BaDVnAz'
 
-@client.event
-async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
-    await client.change_presence(game=discord.Game(name='chat with me!'))
+requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'kurumi'})
 
-@client.event
-async def on_message(message):
-    if not message.author.bot and (message.server == None or client.user in message.mentions):
-        await client.send_typing(message.channel)
-        txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
-        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'kurumi', 'text':txt}).text)
-        if r['status'] == 'success':
-            await client.send_message(message.channel, r['response'] )
+
             
 class Selfbot(commands.Bot):
     '''
@@ -179,6 +169,8 @@ class Selfbot(commands.Bot):
         ---------------
         '''))
         
+        
+
         await self.change_presence(status=discord.Status.invisible, afk=True)
 
     async def on_command(self, ctx):
@@ -256,5 +248,4 @@ class Selfbot(commands.Bot):
 if __name__ == '__main__':
     Selfbot.init()
     
-    print('Starting... cleverbot')
-requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'kurumi'})
+    
